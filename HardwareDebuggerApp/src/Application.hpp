1589b52cf9e5fa065f9bcdb3c1e387e0d26e7718
JesusKrists <krists.n99@gmail.com>
2021-03-30 23:21:06 +0300

    Added log

--- HardwareDebuggerApp/src/Application.hpp
+++ HardwareDebuggerApp/src/Application.hpp
@@ -2,22 +2,37 @@
 
 #include <SDL.h>
 
+int main(int argc, char** argv);
+
 namespace HWD {
 
     class Application {
     public:
-        Application(int argc, char** argv);
-        ~Application();
+        Application(int argc, char** argv, const std::string& name = "HWD App");
+        virtual ~Application();
 
+        //inline Window& GetWindow() {
+        //    return *m_Window;
+        //}
+
+        void Close();
+
+        static inline Application& Get() {
+            return *s_Instance;
+        }
+
+    private:
         void Run();
 
     private:
-        bool Initialize_SDL();
+        //std::unique_ptr<Window> m_Window;
+        bool m_Running        = true;
+        bool m_Minimized      = false;
+        float m_LastFrameTime = 0.0f;
 
     private:
-        bool m_Running = true;
-
-        SDL_Window* m_MainWindow = nullptr;
+        static Application* s_Instance;
+        friend int ::main(int argc, char** argv);
     };
 
 } // namespace HWD
---
+++ HardwareDebuggerApp/src/Compiler.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#if defined(HWD_PLATFORM_WINDOWS)
+#define HWD_DEBUGBREAK() __debugbreak()
+#elif defined(HWD_PLATFORM_LINUX)
+#include <signal.h>
+#define HWD_DEBUGBREAK() raise(SIGTRAP)
+#else
+#error "Platform not defined"
+#endif
