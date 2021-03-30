237f019e62d3a1936d868623ad1e340a99f0f78d
RVeips <rihardsveips@gmail.com>
2021-03-31 00:20:01 +0300

    jlink lib prep

--- HardwareDebuggerApp/src/Probe/JLink.cpp
+++ HardwareDebuggerApp/src/Probe/JLink.cpp
@@ -6,7 +6,23 @@
 
 namespace HWD::Probe {
 
+    static bool s_JLink_Link_Initialized = false;
+
+    static void InitializeLibrary() {
+        if (s_JLink_Link_Initialized)
+            return;
+
+        s_JLink_Link_Initialized = true;
+        auto lib                 = _JLink_LoadLibrary();
+
+        if (lib)
+            HWDLOG_PROBE_INFO("Loaded JLink_x64 library");
+        else
+            HWDLOG_PROBE_CRITICAL("Failed to load JLink_x64 library");
+    }
+
     JLink::JLink() {
+        InitializeLibrary();
         HWDLOG_PROBE_TRACE("JLink::JLink() [{0}]", fmt::ptr(this));
     }
 
