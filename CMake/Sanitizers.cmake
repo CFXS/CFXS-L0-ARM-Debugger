6b76e659625318f35b06c7fea14095c629222c39
Krists Niedritis <kristsn@na.lv>
2021-03-30 09:45:58 +0300

    Fixed SDL2 static bois

--- CMake/Sanitizers.cmake
+++ CMake/Sanitizers.cmake
@@ -2,7 +2,7 @@ function(enable_sanitizers project_name)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL
                                              "Clang")
-    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
+    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" OFF)
 
     if(ENABLE_COVERAGE)
       target_compile_options(project_options INTERFACE --coverage -O0 -g)
@@ -11,23 +11,23 @@ function(enable_sanitizers project_name)
 
     set(SANITIZERS "")
 
-    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" FALSE)
+    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
     if(ENABLE_SANITIZER_ADDRESS)
       list(APPEND SANITIZERS "address")
     endif()
 
-    option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" FALSE)
+    option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
     if(ENABLE_SANITIZER_MEMORY)
       list(APPEND SANITIZERS "memory")
     endif()
 
     option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR
-           "Enable undefined behavior sanitizer" FALSE)
+           "Enable undefined behavior sanitizer" OFF)
     if(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR)
       list(APPEND SANITIZERS "undefined")
     endif()
 
-    option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" FALSE)
+    option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
     if(ENABLE_SANITIZER_THREAD)
       list(APPEND SANITIZERS "thread")
     endif()
