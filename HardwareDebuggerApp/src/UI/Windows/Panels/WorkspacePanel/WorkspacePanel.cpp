ae2605b6e36aa1dff090282c29517722b8509a18
RVeips <rihardsveips@gmail.com>
2021-10-23 04:11:12 +0300

    style stuff + check string before root path set

--- HardwareDebuggerApp/src/UI/Windows/Panels/WorkspacePanel/WorkspacePanel.cpp
+++ HardwareDebuggerApp/src/UI/Windows/Panels/WorkspacePanel/WorkspacePanel.cpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Hardware Debugger <https://github.com/CFXS/CFXS-Hardware-Debugger>
 // Copyright (C) 2021 | CFXS
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -85,7 +85,7 @@ namespace HWD::UI {
     };
 
     void WorkspacePanel::SetRootPath(const QString& path) {
-        if (QDir().exists(path)) {
+        if (!path.isEmpty() && QDir().exists(path)) {
             m_FB_Model->setRootPath(path);
             ui->tw_FileBrowser->setRootIndex(m_FB_Model->index(path));
             ui->tw_FileBrowser->setVisible(true);
