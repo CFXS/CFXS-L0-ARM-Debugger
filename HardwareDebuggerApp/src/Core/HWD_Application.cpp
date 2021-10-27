cd1d3640a5475999c9229b77a250eba80c95de46
RVeips <rihardsveips@gmail.com>
2021-10-27 20:28:14 +0300

    update lhg
    Signed-off-by: RVeips <rihardsveips@gmail.com>

--- HardwareDebuggerApp/src/Core/HWD_Application.cpp
+++ HardwareDebuggerApp/src/Core/HWD_Application.cpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Hardware Debugger <https://github.com/CFXS/CFXS-Hardware-Debugger>
 // Copyright (C) 2021 | CFXS
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -32,16 +32,7 @@ namespace HWD {
 
     void HWD_Application::OnCreate() {
         QFontDatabase::addApplicationFont(":/Font/Code");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-Bold");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-BoldItalic");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-ExtraBold");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-ExtraBoldItalic");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-Italic");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-Light");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-LightItalic");
         QFontDatabase::addApplicationFont(":/Font/OpenSans-Regular");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-SemiBold");
-        QFontDatabase::addApplicationFont(":/Font/OpenSans-SemiBoldItalic");
 
         ProjectManager::HWD_Load();
         Load_Probe();
