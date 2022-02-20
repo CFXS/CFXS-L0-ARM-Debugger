1e0981fb58447f9308aa7d13911216bd9244e067
RVeips <rihardsveips@gmail.com>
2022-02-21 00:46:03 +0200

    refac

--- CFXS_L0_Debugger/src/UI/Panels/AppLogPanel/AppLogPanel.hpp
+++ CFXS_L0_Debugger/src/UI/Panels/AppLogPanel/AppLogPanel.hpp
@@ -1,25 +1,21 @@
 // ---------------------------------------------------------------------
 // CFXS L0 ARM Debugger <https://github.com/CFXS/CFXS-L0-ARM-Debugger>
 // Copyright (C) 2022 | CFXS
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
 // [CFXS] //
-
-// State save/load of TextEditPanel
-// - Current file path is stored in objectName - "TextEditPanel|FilePath"
-// - Current line pos is stored in QSettings (I_Panel::SavePanelState and I_Panel::LoadPanelState)
 
 #pragma once
 
@@ -27,42 +23,34 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QTextEdit>
-#include <UI/Windows/Panels/I_Panel.hpp>
+#include <UI/Panels/I_Panel.hpp>
 
 namespace Ui {
-    class TextEditPanel;
+    class AppLogPanel;
 }
 
 namespace L0::UI {
 
-    class TextEditPanel : public ads::CDockWidget, public I_Panel {
+    class AppLogPanel : public ads::CDockWidget, public I_Panel {
         Q_OBJECT
     public:
-        TextEditPanel();
-        virtual ~TextEditPanel() = default;
+        AppLogPanel();
+        virtual ~AppLogPanel() = default;
 
         /// Get panel base name (for type matching from string)
         static const QString& GetPanelBaseName() {
-            static const QString name = QStringLiteral("TextEditPanel");
+            static const QString name = QStringLiteral("AppLogPanel");
             return name;
         }
 
         void SavePanelState(QSettings* cfg) override;
         void LoadPanelState(QSettings* cfg) override;
 
-        void SetFilePath(const QString& filePath);
-        const QString& GetFilePath() const;
+    private:
+        void FormatAndAppend(const QString& str);
 
     private:
-        void UpdateContent();
-
-    private:
-        std::unique_ptr<Ui::TextEditPanel> ui;
-        QString m_FilePath;
-        QFile m_File;
-        QFileInfo m_FileInfo;
-        QTextEdit* m_TextEdit;
-        QTextEdit* m_LineWidget;
+        std::unique_ptr<Ui::AppLogPanel> ui;
     };
 
 } // namespace L0::UI
--- CFXS_L0_Debugger/src/UI/Panels/TextEditPanel/TextEditPanel.hpp
+++ CFXS_L0_Debugger/src/UI/Panels/TextEditPanel/TextEditPanel.hpp
@@ -1,21 +1,25 @@
 // ---------------------------------------------------------------------
 // CFXS L0 ARM Debugger <https://github.com/CFXS/CFXS-L0-ARM-Debugger>
 // Copyright (C) 2022 | CFXS
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
 // [CFXS] //
+
+// State save/load of TextEditPanel
+// - Current file path is stored in objectName - "TextEditPanel|FilePath"
+// - Current line pos is stored in QSettings (I_Panel::SavePanelState and I_Panel::LoadPanelState)
 
 #pragma once
 
@@ -23,34 +27,42 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QTextEdit>
-#include <UI/Windows/Panels/I_Panel.hpp>
+#include <UI/Panels/I_Panel.hpp>
 
 namespace Ui {
-    class AppLogPanel;
+    class TextEditPanel;
 }
 
 namespace L0::UI {
 
-    class AppLogPanel : public ads::CDockWidget, public I_Panel {
+    class TextEditPanel : public ads::CDockWidget, public I_Panel {
         Q_OBJECT
     public:
-        AppLogPanel();
-        virtual ~AppLogPanel() = default;
+        TextEditPanel();
+        virtual ~TextEditPanel() = default;
 
         /// Get panel base name (for type matching from string)
         static const QString& GetPanelBaseName() {
-            static const QString name = QStringLiteral("AppLogPanel");
+            static const QString name = QStringLiteral("TextEditPanel");
             return name;
         }
 
         void SavePanelState(QSettings* cfg) override;
         void LoadPanelState(QSettings* cfg) override;
 
-    private:
-        void FormatAndAppend(const QString& str);
+        void SetFilePath(const QString& filePath);
+        const QString& GetFilePath() const;
 
     private:
-        std::unique_ptr<Ui::AppLogPanel> ui;
+        void UpdateContent();
+
+    private:
+        std::unique_ptr<Ui::TextEditPanel> ui;
+        QString m_FilePath;
+        QFile m_File;
+        QFileInfo m_FileInfo;
+        QTextEdit* m_TextEdit;
+        QTextEdit* m_LineWidget;
     };
 
 } // namespace L0::UI
